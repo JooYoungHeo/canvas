cc2cd70fc808eaa8779df276922a4a1e426d2f49
JooYoungHeo <jyhur900226@naver.com>
2017-09-24 14:53:55 +0900

    [작업] text2png module test

---
+++ test.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let text2png = require('text2png');
+
+//fs.writeFileSync('out.png', text2png('Hello!', {textColor: 'blue'}));
+
+fs.writeFileSync('test.jpg', text2png('Example\nText', {
+	font: '80px Futura', 
+	textColor: 'teal',
+	bgColor: 'linen',
+	lineSpacing: 10,
+	padding: 20
+}));
