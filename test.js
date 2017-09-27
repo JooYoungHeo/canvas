fdcdcfc56e5fe2d6064ebfac00e0b9ae7744a24e
JooYoungHeo <jyhur900226@naver.com>
2017-09-27 23:22:16 +0900

    [작업] text2png - sample data 로 테스트(한글 안됨)

--- test.js
+++ test.js
@@ -3,8 +3,12 @@ let text2png = require('text2png');
 
 //fs.writeFileSync('out.png', text2png('Hello!', {textColor: 'blue'}));
 
-fs.writeFileSync('test.png', text2png('Example\nText', {
-	font: '80px Futura', 
+let line = 'line1';
+let northbound = 'Arrives in 3 minutes';
+let southbound = 'Arrives in 5 minutes';
+
+fs.writeFileSync('test.png', text2png(`${line}\n${northbound}\n${southbound}`, {
+	font: '40px Futura', 
 	textColor: 'teal',
 	bgColor: 'linen',
 	lineSpacing: 10,
